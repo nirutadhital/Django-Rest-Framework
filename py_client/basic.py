b5a967819cd18cff4f418aaa499b5f38f9a812b5
nirutadhital <dhitalniruta99@gmail.com>
2025-01-21 09:41:40 +0545

    feat: implement pai view

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,14 +1,17 @@
 import requests
 
 # endpoint="https://httpbin.org/status/200/"
-endpoint="https://httpbin.org/anything"
+# endpoint="https://httpbin.org/anything"
+endpoint="http://localhost:8000/api/"
 
 get_response=requests.get(endpoint, data={"query":"Hello world!"})  #API
 print(get_response.text)
+print(get_response.status_code)
+
 
 
 # http request --> html
 # rest api http request-->json
 
-print(get_response.json())
-print(get_response.status_code)
+print(get_response.json()['message'])
+# print(get_response.status_code)
