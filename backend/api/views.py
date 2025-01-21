22655d2ef845bba962e648c66edcc9b3e9ce42c9
nirutadhital <dhitalniruta99@gmail.com>
2025-01-21 12:48:32 +0545

    feat: echo get data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import json
+
 
 # Create your views here.
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message":"hi, there this is Niruta"})
+    #request-> httpRequest->django
+    #print(dir(request))
+    #request.body
+    print(request.GET)
+    print(request.POST)
+    body=request.body
+    data={}#dict
+    try:
+        data=json.loads(body)# string of json data to python dict
+    except:
+        pass
+    print(data.keys())
+    # data['headers']=request.headers  #request.META-->
+    data['params']=dict(request.GET)
+    print(request.headers)
+    data['headers']=dict(request.headers)
+    data['content_type']=request.content_type
+    return JsonResponse(data)
+    # return JsonResponse({"message":"hi, there this is Niruta"})
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,14 +4,15 @@ import requests
 # endpoint="https://httpbin.org/anything"
 endpoint="http://localhost:8000/api/"
 
-get_response=requests.get(endpoint, data={"query":"Hello world!"})  #API
-print(get_response.text)
-print(get_response.status_code)
+# get_response=requests.get(endpoint, params={"abc":123} ,data={"query":"Hello world!"})  #API 
+get_response=requests.get(endpoint, params={"product_id":123})  #API 
+# print(get_response.text)
+# print(get_response.status_code)
 
 
 
 # http request --> html
 # rest api http request-->json
 
-print(get_response.json()['message'])
+print(get_response.json())
 # print(get_response.status_code)
