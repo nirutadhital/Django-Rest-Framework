6dd5758b356f39334c0b22eecb1ea288d795b811
nirutadhital <dhitalniruta99@gmail.com>
2025-01-27 10:10:46 +0545

    feat: create generic api view

--- backend/products/views.py
+++ backend/products/views.py
@@ -3,8 +3,35 @@ from .models import Product
 from .serializers import ProductSerializer
 
 
+
+class ProductCreateAPIView(generics.CreateAPIView):
+    queryset=Product.objects.all()
+    serializer_class=ProductSerializer
+    
+    
+    def perform_create(self, serializer):
+        # serializer.save(user=self.request.user)
+        # print(serializer.validated_data)
+        title=serializer.validated_data.get('title')
+        content=serializer.validated_data.get('content') or None
+        if content is None:
+            content=title
+        serializer.save(content=content)
+        # send a Django signal
+        
+    
+product_create_view=ProductCreateAPIView.as_view()
+    
+
+
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset=Product.objects.all()
     serializer_class=ProductSerializer
     #lookuop_field='pk
+
+product_detail_view=ProductDetailAPIView.as_view()
+
+
+
+
     
---
+++ py_client/create.py
@@ -0,0 +1,13 @@
+import requests
+
+endpoint="http://localhost:8000/api/products/"
+
+data={
+    "title":"This field is done",
+    "price":32.99
+}
+
+get_response=requests.post(endpoint, json=data)  #API f
+
+
+print(get_response.json())
