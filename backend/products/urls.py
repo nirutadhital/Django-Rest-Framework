dc8aa2264bf874d1d96733f6da7d82f6d82faa92
nirutadhital <dhitalniruta99@gmail.com>
2025-01-28 11:14:19 +0545

    feat: implement function based views

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -4,4 +4,6 @@ urlpatterns = [
     # path('',views.product_create_view),
     path('',views.product_list_create_view),
     path('<int:pk>/',views.product_detail_view)
+    # path('',views.product_alt_view),
+    # path('<int:pk>/',views.product_alt_view)
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,6 +1,10 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
+from django.shortcuts import get_object_or_404
+# from django.http import Http404
 
 
 
@@ -8,18 +12,14 @@ class ProductCreateAPIView(generics.CreateAPIView):
     queryset=Product.objects.all()
     serializer_class=ProductSerializer
     
-    
     def perform_create(self, serializer):
-        # serializer.save(user=self.request.user)
-        # print(serializer.validated_data)
         title=serializer.validated_data.get('title')
         content=serializer.validated_data.get('content') or None
         if content is None:
             content=title
         serializer.save(content=content)
         # send a Django signal
-        
-    
+          
 product_create_view=ProductCreateAPIView.as_view()
     
 
@@ -51,18 +51,46 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
 product_list_create_view=ProductListCreateAPIView.as_view()
 
 
-class ProductListAPIView(generics.ListAPIView):
-    '''
-    Not gonna use this method
-    '''
-    queryset=Product.objects.all()
-    serializer_class=ProductSerializer
+# class ProductListAPIView(generics.ListAPIView):
+#     '''
+#     Not gonna use this method
+#     '''
+#     queryset=Product.objects.all()
+#     serializer_class=ProductSerializer
     
-product_list_view=ProductListAPIView.as_view()
+# product_list_view=ProductListAPIView.as_view()
 
     
 
 
+@api_view(['GET','POST'])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method=request.method
+    
+    if method=="GET":
+        if pk is not None:
+            # detail view
+            obj=get_object_or_404(Product, pk=pk)#
+            data=ProductSerializer(obj, many=False).data        
+            return Response(data)
+        #url_args??
+        # get request ->detail view
+        # list view
+        queryset=Product.objects.all()
+        data=ProductSerializer(queryset, many=True).data
+        return Response(data)
+    
+    if method=="POST":
+        serializer=ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            title=serializer.validated_data.get('title')
+            content=serializer.validated_data.get('content') or None
+            if content is None:
+                content=title
+            serializer.save(content=content)
+        return Response(serializer.data)
+    return Response({"invalid":"not good data"},status=400)
 
-
+   
+    
     
