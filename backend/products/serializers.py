3e4aef65428555a614f99b2db731c25ab9d1dca2
nirutadhital <dhitalniruta99@gmail.com>
2025-02-03 11:29:03 +0545

    feat: Urls, Reverse and Serializers

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -2,14 +2,22 @@
 # it converts models into dict
 
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 
 from products.models import Product
 
 class ProductSerializer(serializers.ModelSerializer):  
     my_discount=serializers.SerializerMethodField(read_only=True)
+    edit_url=serializers.SerializerMethodField(read_only=True)
+    url=serializers.HyperlinkedIdentityField(
+        view_name='product-detail',
+        lookup_field='pk'
+    )
     class Meta:
         model = Product
         fields = [
+            'url',
+            'edit_url',
             'pk',
             'title',
             'content',
@@ -17,6 +25,12 @@ class ProductSerializer(serializers.ModelSerializer):
             'sale_price',
             'my_discount',
         ]
+        
+    def get_edit_url(self, obj):
+        request = self.context.get('request') # self.request
+        if request is None:
+            return None
+        return reverse("product-edit", kwargs={"pk": obj.pk}, request=request)     
         
         
     def get_my_discount(self, obj):
